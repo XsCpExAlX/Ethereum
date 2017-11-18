26f9c0d86358f7c3b2f9d2c3fc7bd70c1da15784
donutkastle <donutkastle@gmail.com>
2017-11-18 11:18:32 -0800

    rnn class
    fixed some bugs

--- GDAX RNN/ccxt_rnn_class.py
+++ GDAX RNN/ccxt_rnn_class.py
@@ -31,9 +31,13 @@ class model_RNN:
 
     def train_and_predict(self, restore=False, data_rnn=None, data_rnn_ckpt=None):
         tf.reset_default_graph()
+        print('Restore model? %s' % restore)
+        print('nrows of raw data= %s' % len(data_rnn.index))
 
         # Drop first few rows of data because for some reason prices are 0.00
         data_rnn = data_rnn.drop(data_rnn.index[0:2])
+        print('nrows with dropped initial zeros= %s' % len(data_rnn.index))
+
 
         # Convert 'trades_date_time' and  'order_date_time' from object to datetime object
         data_rnn['trades_date_time'] = pd.to_datetime(data_rnn['trades_date_time'])
@@ -54,6 +58,7 @@ class model_RNN:
         # Resample data by setting index to 'trades_date_time' to avoid repeats
         data_rnn = data_rnn[data_rnn.columns.difference(['order_date_time'])]  # .difference() method removes any columns and automatically reorders columns alphanumerically
         data_rnn = data_rnn.resample('S', on='trades_date_time').mean().interpolate(method='linear')
+        print('nrows with resample = %s' % len(data_rnn.index))
 
 
         # RNN Hyperparams
@@ -72,8 +77,7 @@ class model_RNN:
         PriceRange = data_rnn['trade_px'].max() - data_rnn['trade_px'].min()
         PriceMean = data_rnn['trade_px'].mean()
         data_rnn_norm = (data_rnn - data_rnn.mean()) / (data_rnn.max() - data_rnn.min())
-        # print(data_rnn['trade_px'].max())
-        # print(data_rnn.max())
+        # print(data_rnn_norm.tail(3))
 
 
         # Pick all apporpriate columns to train and test in RNN
@@ -96,16 +100,23 @@ class model_RNN:
                                             name='target_ph')
 
         # RNN Train-Test Split
-        for i in range(min_test_size, len(data_rnn.index)):
-            if (i % truncated_backprop_length * batch_size == 0):
-                test_first_idx = len(data_rnn.index) - i
-                break
-        # Purposefully uses self.data_rnn['row_nums'] because self.data_rnn['row_nums'] also becomes normalized
-        data_rnn_train = data_rnn_norm[data_rnn['row_num'] < test_first_idx]
-        data_rnn_test = data_rnn_norm[data_rnn['row_num'] >= test_first_idx]
+        if restore:
+            data_rnn_test = data_rnn_norm
+            print('nrows of testing = %s' % len(data_rnn_test.index))
 
-        xTrain = data_rnn_train[rnn_column_list].as_matrix()
-        yTrain = data_rnn_train[['future_ma_%s' % self.future_ma_window]].as_matrix()
+        else:
+            for i in range(min_test_size, len(data_rnn.index)):
+                if (i % truncated_backprop_length * batch_size == 0):
+                    test_first_idx = len(data_rnn.index) - i
+                    break
+            # Purposefully uses data_rnn['row_nums'] because data_rnn['row_nums'] also becomes normalized
+            data_rnn_train = data_rnn_norm[data_rnn['row_num'] < test_first_idx]
+            print('nrows of training = %s' % len(data_rnn_train.index))
+            data_rnn_test = data_rnn_norm[data_rnn['row_num'] >= test_first_idx]
+            print('nrows of testing = %s' % len(data_rnn_test.index))
+
+            xTrain = data_rnn_train[rnn_column_list].as_matrix()
+            yTrain = data_rnn_train[['future_ma_%s' % self.future_ma_window]].as_matrix()
         xTest = data_rnn_test[rnn_column_list].as_matrix()
         yTest = data_rnn_test[['future_ma_%s' % self.future_ma_window]].as_matrix()
 
@@ -240,7 +251,7 @@ class model_RNN:
 if __name__ == '__main__':
     # Read DataFrame.csv
     data_rnn = pd.read_csv('C:/Users/donut/PycharmProjects/backtrader/backtrader-master/datas/ETHUSD2_pandas_rnn_prepared_simplified.csv', nrows=50000)
-    new_data_rnn = pd.read_csv('C:/Users/donut/PycharmProjects/backtrader/backtrader-master/datas/ETHUSD2_pandas_rnn_prepared_simplified.csv', nrows=150000)
+    new_data_rnn = pd.read_csv('C:/Users/donut/PycharmProjects/backtrader/backtrader-master/datas/ETHUSD2_pandas_rnn_prepared_simplified.csv', nrows=5000)
     data_rnn_ckpt = 'C:/Users/donut/PycharmProjects/backtrader/backtrader-master/rnn_saved_models/testing1'
     x = model_RNN(30, 1, 5, 5, num_epochs=30)
     x.train_and_predict(restore=True, data_rnn=new_data_rnn, data_rnn_ckpt=data_rnn_ckpt)
