950b3c508c945d4d8d4ab3e7e9d685a6ea51bb30
donutkastle <donutkastle@gmail.com>
2017-11-22 01:28:09 -0800

    execution limit with viable trading strategy
    You can test it out with regular and reversed data.

--- GDAX RNN/execution_limit.py
+++ GDAX RNN/execution_limit.py
@@ -8,12 +8,36 @@ import sys  # To find out the script name (in argv[0])
 # Import the backtrader platform
 import backtrader as bt
 
+import datetime
+import backtrader.feeds as btfeed
+
+class RNN (bt.feeds.GenericCSVData):
+
+  params = (
+    ('fromdate', datetime.datetime(2017, 1, 1)),
+    ('todate', datetime.datetime(2018, 12, 31)),
+    ('nullvalue', 0.0),
+    ('dtformat', ('%Y%m%d %H:%M:%S.%f')),
+    ('tmformat', ('%H.%M:%S')),
+
+    ('datetime', 0),
+    ('time', -1),
+    ('open', 1),
+    ('high', 2),
+    ('low', 3),
+    ('close', 4),
+    ('RNN', 5),
+    ('volume', 6),
+    ('openinterest', -1),
+
+)
+
 
 # Create a Stratey
 class TestStrategy(bt.Strategy):
     params = (
-        ('maperiod1', 10),
-        ('maperiod2', 40),
+        ('maperiod1', 5),
+        ('maperiod2', 100),
     )
 
     def log(self, txt, dt=None):
@@ -24,6 +48,7 @@ class TestStrategy(bt.Strategy):
     def __init__(self):
         # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
+        self.dataRNN = self.datas[0].RNN
 
         # To keep track of pending orders and buy price/commission
         self.order = None
@@ -36,12 +61,11 @@ class TestStrategy(bt.Strategy):
 
 
         # Indicators for the plotting show
-        self.ema1 = bt.indicators.ExponentialMovingAverage(self.datas[0], period=self.params.maperiod1)
-        self.ema2 = bt.indicators.ExponentialMovingAverage(self.datas[0], period=self.params.maperiod2)
-
-        self.ema1_minus_ema2 = self.ema1 - self.ema2
+        self.emaRNN1 = bt.indicators.ExponentialMovingAverage(self.datas[0].RNN, period=self.params.maperiod1)
+        self.smaRNN2 = bt.indicators.MovingAverageSimple(self.datas[0].RNN, period=self.params.maperiod2)
+        self.emaRNN1_minus_smaRNN2 = self.emaRNN1 - self.smaRNN2
+        # self.ema1_minus_ema2 = self.ema1 - self.ema2
         # self.ema1_minus_sma1 = self.ema1 - self.sma1
-
         # bt.indicators.WeightedMovingAverage(self.datas[0], period=25,
         #                                     subplot=True)
         # bt.indicators.StochasticSlow(self.datas[0])
@@ -64,18 +88,18 @@ class TestStrategy(bt.Strategy):
                     (order.executed.price,
                      order.executed.value,
                      order.executed.comm))
-
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
+                self.bar_executed = len(self)
+
             else:  # Sell
                 self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
                          (order.executed.price,
                           order.executed.value,
                           order.executed.comm))
+                self.bar_executed = len(self)
 
-            self.bar_executed = len(self)
-
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+        if order.status in [order.Canceled, order.Margin, order.Rejected]:
             self.log('Order Canceled/Margin/Rejected')
 
         # Write down: no pending order
@@ -91,40 +115,32 @@ class TestStrategy(bt.Strategy):
     def next(self):
         # Simply log the closing price of the series from the reference
         # self.log('Close, %.2f, sma[%d] = %.2f, ema[%d] = %.2f' % (self.dataclose[0], self.params.maperiod1, self.sma1[0], self.params.maperiod1, self.ema1[0]))
-        self.log('Close, %.2f, ema1[%d] = %.2f, ema2[%d] = %.2f, ema1_minus_ema2 = %.2f' % (self.dataclose[0], self.params.maperiod1, self.ema1[0], self.params.maperiod2, self.ema2[0], self.ema1_minus_ema2[0]))
+        # self.log('Close, %.2f, RNN, %.2f, ema1[%d] = %.2f, ema2[%d] = %.2f, ema1_minus_ema2 = %.2f' % (self.dataclose[0], self.dataRNN[0], self.params.maperiod1, self.ema1[0], self.params.maperiod2, self.ema2[0], self.ema1_minus_ema2[0]))
+        self.log('Close = %.2f, RNN = %.2f, emaRNN1 minus smaRNN2 = %.2f' % (self.dataclose[0], self.dataRNN[0], self.emaRNN1_minus_smaRNN2[0]))
 
 
         # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
             return
-
-        # Check if we are in the market
         if not self.position:
-
-            # Not yet ... we MIGHT BUY if ...
-            if (self.ema1_minus_ema2[0] > 0 and self.ema1_minus_ema2[-1] < 0):
-            # if self.ema1_minus_ema2 < 0 or self.ema1_minus_ema2[-2] > self.ema1_minus_ema2[-1] < self.ema1_minus_ema2[0] or self.ema1_minus_ema2[-1] < 0 < self.ema1_minus_ema2[0]:
-            # if (self.ema1_minus_ema2[-2] - self.ema1_minus_ema2[-1] > 1 * (self.ema1_minus_ema2[-1] - self.ema1_minus_ema2[0])):
-            # if (self.ema1_minus_ema2[0] > 0 and self.ema1_minus_ema2[-1] < 0):
-
+            if (self.emaRNN1_minus_smaRNN2[0] > 0 and self.emaRNN1[0] < self.dataRNN[0]):
                 # BUY, BUY, BUY!!! (with all possible default parameters)
                 self.log('BUY CREATE, %.2f' % self.dataclose[0])
 
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.buy(exectype=bt.Order.Limit,
-                                      price=self.dataclose[0])
+                                      price=self.dataclose[0]-0.01,
+                                      valid=datetime.timedelta(seconds=30))
 
         else:
-            if (self.ema1_minus_ema2[0] < 0 and self.ema1_minus_ema2[-1] > 0):
-            # if self.ema1_minus_ema2 > 0 or self.ema1_minus_ema2[-1] > 0 > self.ema1_minus_ema2[0]:
-            # if (self.ema1_minus_ema2[-2] - self.ema1_minus_ema2[-1] < 1 * (self.ema1_minus_ema2[-1] - self.ema1_minus_ema2[0])):
-            # if (self.ema1_minus_ema2[0] < 0 and self.ema1_minus_ema2[-1] > 0):
+            if (self.emaRNN1_minus_smaRNN2[0] < 0 and self.emaRNN1[0] > self.dataRNN[0]):
                 # SELL, SELL, SELL!!! (with all possible default parameters)
                 self.log('SELL CREATE, %.2f' % self.dataclose[0])
 
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell(exectype=bt.Order.Limit,
-                                       price=self.dataclose[0])
+                                       price=self.dataclose[0]+0.01,
+                                       valid=datetime.timedelta(seconds=30))
 
 
 if __name__ == '__main__':
@@ -137,10 +153,10 @@ if __name__ == '__main__':
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    datapath = os.path.join(modpath, 'C:/Users/donut/PycharmProjects/backtrader/backtrader-master/datas/ETHUSD_backtrader.csv')
+    datapath = os.path.join(modpath, 'C:/Users/donut/PycharmProjects/backtrader/backtrader-master/datas/ETHUSD_RNN10_bt.csv')
 
     # Create a Data Feed
-    data = bt.feeds.GenericCSVData(
+    data = RNN(
         dataname=datapath,
         # Do not pass values before this date
         sessionstart=datetime.datetime(2017, 9, 17),
@@ -149,14 +165,7 @@ if __name__ == '__main__':
         # Do not pass values after this date
         reverse=False)
 
-    #
-    # datetime=0,
-    # high=1,
-    # low=2,
-    # open=3,
-    # close=4,
-    # volume=5,
-    # openinterest=-1
+
 
     # Add the Data Feed to Cerebro
     cerebro.adddata(data)
@@ -165,9 +174,9 @@ if __name__ == '__main__':
     cerebro.broker.setcash(10000.00)
 
     # Add a FixedSize sizer according to the stake
-    cerebro.addsizer(bt.sizers.PercentSizer, percents=50)
+    cerebro.addsizer(bt.sizers.PercentSizer, percents=20)
 
-    # Set the commission3.
+    # Set the commission (limit order is 0.0000)
     cerebro.broker.setcommission(commission=0.0000)
 
     # Print out the starting conditions
@@ -181,3 +190,4 @@ if __name__ == '__main__':
 
     # Plot the result
     cerebro.plot()
+
