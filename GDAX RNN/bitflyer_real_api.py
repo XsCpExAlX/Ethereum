10316bd4d1adbf60576a0a458d416ef11203d5bc
donutkastle <donutkastle@gmail.com>
2017-12-10 15:48:02 -0800

    bitflyer api

---
+++ GDAX RNN/bitflyer_real_api.py
@@ -0,0 +1,10 @@
+import ccxt
+
+
+access_key = 'MppxYNrFC1YoTGsJ5Jt13e'
+secret_key = 'jVuN1NCgFwtzab1xFnjFsLzX6ugYkcdp4vppPvO4AG0='
+
+
+bitflyer = ccxt.bitflyer()
+bitflyer.access_key = access_key
+bitflyer.secret_key = secret_key
